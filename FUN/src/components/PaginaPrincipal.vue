6958421830c210234a1982afd2c06771ed0b8b4e
Aylin67 <158499297+Aylin67@users.noreply.github.com>
2025-03-24 14:25:02 -0600

    PAGINA
    CAFETERIA

---
+++ FUN/src/components/PaginaPrincipal.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+import HeaderBar from './HeaderBar.vue';
+
+import Menu from './Menu.vue';
+import Carosel from './Carosel.vue';
+import cardPostres from './cardPostres.vue';
+import seccion from './seccion.vue';
+import cardOfertas from './cardOfertas.vue';
+import formulario from './formulario.vue';
+import piepagina from './piepagina.vue';
+
+</script>
+
+<template>
+  <!-- encabezado  -->
+<HeaderBar/>
+  <!-- creamos un menu -->
+  <Menu/>
+ <!-- utilizamos un carrucel -->
+  <Carosel/>
+  <!-- un menu de potres -->
+  <cardPostres/>
+     <!-- seccion -->
+  <seccion/>
+  <!-- OFERTAS con row, card-->
+   <cardOfertas/>
+   <!-- contacto formulario -->
+   <formulario/>
+<!-- pie de pagina -->
+ <piepagina/>
+  
+
+
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ FUN/src/components/piepagina.vue
@@ -0,0 +1,2 @@
+<footer style="background-color: black; color:#fff;" class="text-center py-3 ">@DWB-Todos los derechos reservados
+</footer>
