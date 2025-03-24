9ebe1d0169cc3affe6bbef9ccc82e5e675bdb0c0
Aylin67 <158499297+Aylin67@users.noreply.github.com>
2025-03-24 14:48:06 -0600

    pagina
    completa

--- FUN/src/components/piepagina.vue
+++ FUN/src/components/piepagina.vue
@@ -1,2 +1,22 @@
-<footer style="background-color: black; color:#fff;" class="text-center py-3 ">@DWB-Todos los derechos reservados
-</footer>
+
+<template>
+    <footer style="background-color: black; color:#fff;" class="text-center py-3 ">@DWB-Todos los derechos reservados
+    </footer>
+   
+  </template>
+  
+  <script>
+
+  export default {
+    name: "PiePagina"
+  };
+  </script>
+  
+  <style scoped>
+  footer {
+    text-align: center;
+    padding: 10px;
+    background-color: #f8f8f8;
+  }
+  </style>
+  
